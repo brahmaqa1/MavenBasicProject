01a63688cf591cec73b0f0b3e4ee04702b8ef651
brahmaqa1 <brahma.qa1@gmail.com>
2022-01-31 20:58:46 +0530

    added new test case

---
+++ src/test/java/MyTests/AndroidTest2.java
@@ -0,0 +1,19 @@
+package MyTests;
+import org.testng.annotations.Test;
+
+public class AndroidTest2 {
+
+	@Test
+	public void testA()
+	{	
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("******************************************");
+		System.out.println("Step 1: Executing Android test -step1");
+		System.out.println("Step2 : executing step 2 ");
+		System.out.println("hi");
+		
+		System.out.println("******************************************");
+	}
+
+}
